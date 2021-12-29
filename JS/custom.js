9a3f67a8d8d9e7ad0706b75a5b599896ea2d7ae8
majd-alaarg <67345864+majd-alaarg@users.noreply.github.com>
2021-12-29 15:19:45 -0500

    Create custom.js

---
+++ JS/custom.js
@@ -0,0 +1,20 @@
+$(function () {
+	// PROJECT SLIDE
+	$('#project-slide').owlCarousel({
+		loop: true,
+		center: true,
+		autoplayHoverPause: false,
+		autoplay: true,
+		margin: 30,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			768: {
+				items: 2,
+			}
+		}
+	});
+
+});
