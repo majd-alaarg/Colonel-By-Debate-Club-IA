a50246d9d2e11af560e3eb9b8c55dabf80901d56
majd-alaarg <67345864+majd-alaarg@users.noreply.github.com>
2022-01-01 19:02:01 -0500

    Create active.js

---
+++ JS/active.js
@@ -0,0 +1,62 @@
+(function ($) {
+    'use strict';
+
+    var browserWindow = $(window);
+	
+    //Animated competitions slider
+    if ($.fn.owlCarousel) {
+        var welcomeSlide = $('.hero-post-slides');
+        var upcomingSlides = $('.upcoming-slides');
+
+        welcomeSlide.owlCarousel({
+            items: 1,
+            margin: 0,
+            loop: true,
+            nav: false,
+            dots: false,
+            autoplay: true,
+            center: true,
+            autoplayTimeout: 7000,
+            smartSpeed: 1000
+        });
+
+        welcomeSlide.on('translate.owl.carousel', function () {
+            var slideLayer = $("[data-animation]");
+            slideLayer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).removeClass('animated ' + anim_name).css('opacity', '0');
+            });
+        });
+
+        welcomeSlide.on('translated.owl.carousel', function () {
+            var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
+            slideLayer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).addClass('animated ' + anim_name).css('opacity', '1');
+            });
+        });
+
+        $("[data-delay]").each(function () {
+            var anim_del = $(this).data('delay');
+            $(this).css('animation-delay', anim_del);
+        });
+
+        $("[data-duration]").each(function () {
+            var anim_dur = $(this).data('duration');
+            $(this).css('animation-duration', anim_dur);
+        });
+
+        upcomingSlides.owlCarousel({
+            items: 1,
+            margin: 0,
+            loop: true,
+            nav: true,
+            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 7000,
+            smartSpeed: 1500
+        });
+    }
+
+})(jQuery);
