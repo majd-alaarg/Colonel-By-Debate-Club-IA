57c5037514629631b8cd57f51265db4ea7e7aa4f
majd-alaarg <67345864+majd-alaarg@users.noreply.github.com>
2022-01-12 20:42:36 -0500

    Updating homepage landing

--- JS/active.js
+++ JS/active.js
@@ -1,62 +1,68 @@
 (function ($) {
-    'use strict';
+	'use strict';
 
-    var browserWindow = $(window);
-	
-    //Animated competitions slider
-    if ($.fn.owlCarousel) {
-        var welcomeSlide = $('.hero-post-slides');
-        var upcomingSlides = $('.upcoming-slides');
+	var browserWindow = $(window);
+	browserWindow.on('load', function () {
+		$('.preloader').fadeOut('slow', function () {
+			$(this).remove();
+		});
+	});
 
-        welcomeSlide.owlCarousel({
-            items: 1,
-            margin: 0,
-            loop: true,
-            nav: false,
-            dots: false,
-            autoplay: true,
-            center: true,
-            autoplayTimeout: 5000,
-            smartSpeed: 1000
-        });
 
-        welcomeSlide.on('translate.owl.carousel', function () {
-            var slideLayer = $("[data-animation]");
-            slideLayer.each(function () {
-                var anim_name = $(this).data('animation');
-                $(this).removeClass('animated ' + anim_name).css('opacity', '0');
-            });
-        });
+	//Animated competitions slider
+	if ($.fn.owlCarousel) {
+		var welcomeSlide = $('.hero-post-slides');
+		var upcomingSlides = $('.upcoming-slides');
 
-        welcomeSlide.on('translated.owl.carousel', function () {
-            var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
-            slideLayer.each(function () {
-                var anim_name = $(this).data('animation');
-                $(this).addClass('animated ' + anim_name).css('opacity', '1');
-            });
-        });
+		welcomeSlide.owlCarousel({
+			items: 1,
+			margin: 0,
+			loop: true,
+			nav: false,
+			dots: false,
+			autoplay: true,
+			center: true,
+			autoplayTimeout: 5000,
+			smartSpeed: 1000
+		});
 
-        $("[data-delay]").each(function () {
-            var anim_del = $(this).data('delay');
-            $(this).css('animation-delay', anim_del);
-        });
+		welcomeSlide.on('translate.owl.carousel', function () {
+			var slideLayer = $("[data-animation]");
+			slideLayer.each(function () {
+				var anim_name = $(this).data('animation');
+				$(this).removeClass('animated ' + anim_name).css('opacity', '0');
+			});
+		});
 
-        $("[data-duration]").each(function () {
-            var anim_dur = $(this).data('duration');
-            $(this).css('animation-duration', anim_dur);
-        });
+		welcomeSlide.on('translated.owl.carousel', function () {
+			var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
+			slideLayer.each(function () {
+				var anim_name = $(this).data('animation');
+				$(this).addClass('animated ' + anim_name).css('opacity', '1');
+			});
+		});
 
-        upcomingSlides.owlCarousel({
-            items: 1,
-            margin: 0,
-            loop: true,
-            nav: true,
-            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-            dots: false,
-            autoplay: true,
-            autoplayTimeout: 5000,
-            smartSpeed: 1500
-        });
-    }
+		$("[data-delay]").each(function () {
+			var anim_del = $(this).data('delay');
+			$(this).css('animation-delay', anim_del);
+		});
+
+		$("[data-duration]").each(function () {
+			var anim_dur = $(this).data('duration');
+			$(this).css('animation-duration', anim_dur);
+		});
+
+		upcomingSlides.owlCarousel({
+			items: 1,
+			margin: 0,
+			loop: true,
+			nav: true,
+			navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+			dots: false,
+			autoplay: true,
+			autoplayTimeout: 5000,
+			smartSpeed: 1500
+		});
+	}
 
 })(jQuery);
